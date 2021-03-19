671c2f9638073c84de8916e0444e9cdce3d55190
Veer <40777269+V3RS@users.noreply.github.com>
2021-03-19 16:28:41 -0400

    review table

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,4 @@
 from .db import db
 from .user import User
 from .spot import Spot
+from .review import Review
---
+++ app/models/review.py
@@ -0,0 +1,13 @@
+from .db import db
+from sqlalchemy.orm import relationship
+
+
+class Review(db.Model):
+
+    __tablename__ = 'reviews'
+
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.Text, nullable=False)
+    rating = db.Column(db.Integer)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    user = relationship("User", back_populates='reviews')
