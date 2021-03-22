339515bd1a0f1a8d7f6d3e234de4f604779fe9dc
cygnus33065 <cygnus_33065@yahoo.com>
2021-03-22 19:38:03 -0400

    more aws upload work- not complete not able to auth to aws

--- app/api/photo_uploads.py
+++ app/api/photo_uploads.py
@@ -3,11 +3,14 @@ import logging
 import boto3
 from botocore.exceptions import ClientError
 from flask import Blueprint, jsonify
-from flask_login import login_required
 from app.models import User, Picture
 
-photo_routes = Blueprint('photo_upload', __name__)
 
-@photo_routes.route('/', methods=['POST'])
-@login_required
+s3 = boto3.client('s3')
+photo_routes = Blueprint('/photo_upload', __name__)
+
+
+@photo_routes.route('/upload', methods=['POST'])
 def photo():
+  uploaded_photo = s3.upload_file(Filename='app/test_upload/best-zoom-backgrounds-disney-1586899397.jpg', Bucket="skybnb", Key='test1')
+  print(uploaded_photo)
