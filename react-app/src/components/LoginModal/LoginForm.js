1a2ed9878f1ed6141fcb49c76c033bcfd8caaecc
Veer <40777269+V3RS@users.noreply.github.com>
2021-03-22 19:48:56 -0400

    log in form styling

--- react-app/src/components/LoginModal/LoginForm.js
+++ react-app/src/components/LoginModal/LoginForm.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import { Redirect } from "react-router-dom";
 import { login } from "../../services/auth";
 
+import "./LoginModal.css";
+
 const LoginForm = ({ authenticated, setAuthenticated }) => {
   const [errors, setErrors] = useState([]);
   const [email, setEmail] = useState("");
@@ -30,34 +32,48 @@ const LoginForm = ({ authenticated, setAuthenticated }) => {
   }
 
   return (
-    <form onSubmit={onLogin}>
-      <div>
-        {errors.map((error) => (
-          <div>{error}</div>
-        ))}
-      </div>
-      <div>
-        <label htmlFor="email">Email</label>
-        <input
-          name="email"
-          type="text"
-          placeholder="Email"
-          value={email}
-          onChange={updateEmail}
-        />
-      </div>
-      <div>
-        <label htmlFor="password">Password</label>
-        <input
-          name="password"
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={updatePassword}
-        />
-        <button type="submit">Login</button>
-      </div>
-    </form>
+    <div className="login-container">
+      <div id="login-title">Log in</div>
+      <form onSubmit={onLogin}>
+        <div>
+          {errors.map((error) => (
+            <div>{error}</div>
+          ))}
+        </div>
+        <div>
+          <input
+            name="email"
+            type="text"
+            placeholder="Email"
+            value={email}
+            onChange={updateEmail}
+            className="login-inputs"
+            required
+          />
+        </div>
+        <div>
+          <input
+            name="password"
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={updatePassword}
+            id="login-pw-input"
+            className="login-inputs"
+            required
+          />
+          <button type="submit" className="loginFormBtns">
+            Log in
+          </button>
+        </div>
+        <div className="login-or">
+          <div className="before-or"></div>
+          <div>or</div>
+          <div className="after-or"></div>
+        </div>
+        <button className="loginFormBtns">Demo Log in</button>
+      </form>
+    </div>
   );
 };
 
