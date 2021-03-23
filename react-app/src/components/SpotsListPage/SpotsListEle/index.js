fbb2e3c7cfb9ace4ab3e0df820240744d7ec5204
Tommy Nienchi Chen <tommynchen@gmail.com>
2021-03-23 11:11:16 -0700

    some more styling

--- react-app/src/components/SpotsListPage/SpotsListEle/index.js
+++ react-app/src/components/SpotsListPage/SpotsListEle/index.js
@@ -14,17 +14,21 @@ export default function SpotsListEle({
     <div className="spotslistele">
       <img src={img} alt="" />
       <i class="far fa-heart"></i>
-      <div className="spotslistele-info-top">
-        <p>{location_desc}</p>
-        <h3>{title}</h3>
-        <p>______</p>
-        <p>{description}</p>
-      </div>
-      <div className="spotslistele-info-bot">
-        <i class="fas fa-star">{star}</i>
-        <div className="spotslistele-price-div">
-          <p>${price}/ night</p>
-          <p>${total} total</p>
+
+      <div className="spotslistele-info-container">
+        <div className="spotslistele-info-top">
+          <span>{location_desc}</span>
+          <h3>{title}</h3>
+          <div className="info-top-line"></div>
+          <p>{description}</p>
+        </div>
+
+        <div className="spotslistele-info-bot">
+          <i class="fas fa-star">{star}</i>
+          <div className="spotslistele-price-div">
+            <div>${price}/ night</div>
+            <div>${total} total</div>
+          </div>
         </div>
       </div>
     </div>
--- react-app/src/components/SpotsListPage/index.js
+++ react-app/src/components/SpotsListPage/index.js
@@ -9,23 +9,25 @@ export default function SpotsListPage() {
   return (
     <div className="spotslistpage-container">
       <div className="spotslistpage-spotslist-container">
-        <div className="spotslist-info">
-          <p>Search meta-data here</p>
-        </div>
-        <h3>Search Criteria Display - e.g. Unique Stay</h3>
-        <div className="spotslistpage-filter-btn-container">
-          <button className="spotslistpage-filter-btn">
-            Cancellation flexibility
-          </button>
-          <button className="spotslistpage-filter-btn">Entire place</button>
-          <button className="spotslistpage-filter-btn">Price</button>
-          <button className="spotslistpage-filter-btn">More filters</button>
+        <div className="spotslist-header">
+          <div className="spotslist-info">
+            <p>Search meta-data here</p>
+            <h2>Search Criteria Display - e.g. Unique Stay</h2>
+          </div>
+          <div className="spotslistpage-filter-btn-container">
+            <button className="spotslistpage-filter-btn">
+              Cancellation flexibility
+            </button>
+            <button className="spotslistpage-filter-btn">Entire place</button>
+            <button className="spotslistpage-filter-btn">Price</button>
+            <button className="spotslistpage-filter-btn">More filters</button>
+          </div>
         </div>
         <SpotsListEle
           img="https://images.pexels.com/photos/584399/living-room-couch-interior-room-584399.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
           location_desc="Somewhere in a galaxy far far away"
-          title="modernly decorated room"
-          description="2 bedroom apartment with full amenities"
+          title="Modernly decorated room"
+          description="4 guests · 1 bedroom · 2 beds · 1 bath"
           star={4.5}
           price={42}
           total={126}
