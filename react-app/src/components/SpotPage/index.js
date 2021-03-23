c679ac4570cd928438eefbfd39b7ff5df6441d2b
Veer <40777269+V3RS@users.noreply.github.com>
2021-03-23 16:18:22 -0400

    spot_page starter

--- react-app/src/components/SpotPage/index.js
+++ react-app/src/components/SpotPage/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import "./SpotPage.css";
+import { useParams } from "react-router";
 
 export default function SpotPage() {
+  const { spotId } = useParams();
+
   return (
     <div id="spc">
       <div className="spot-page-container">
         <div className="spot-page-title">
-          <h1>Spot Page Title</h1>
+          <h1>Spot Page Title {spotId}</h1>
         </div>
         <div className="spot-secondHeading">
           <h3>Superhost</h3>
