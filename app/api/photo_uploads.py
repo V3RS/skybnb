e9923cdc588c054176223ebc8697d5f0dfb39a83
cygnus33065 <cygnus_33065@yahoo.com>
2021-03-23 12:12:06 -0400

    more work on photo upload

--- app/api/photo_uploads.py
+++ app/api/photo_uploads.py
@@ -1,16 +1,38 @@
-import os
-import logging
-import boto3
-from botocore.exceptions import ClientError
-from flask import Blueprint, jsonify
-from app.models import User, Picture
+from flask import Blueprint, request
+from app.models import db, Picture
+from flask_login import current_user, login_required
+from app.s3_helpers import (
+    upload_file_to_s3, allowed_file, get_unique_filename)
 
 
-s3 = boto3.client('s3')
+s3 = boto3.client('s3', aws_access_key_id=os.environ.get("S3_KEY"), aws_secret_access_key=os.environ.get('S3_SECRET'))
 photo_routes = Blueprint('/photo_upload', __name__)
+bucket = os.environ.get('S3_BUCKET')
 
+@photo_routes.route('', methods=['POST'])
+@login_required
+def upload_image():
+    if "image" not in request.files:
+        return {"errors": "image required"}, 400
 
-@photo_routes.route('/upload', methods=['POST'])
-def photo():
-  uploaded_photo = s3.upload_file(Filename='app/test_upload/best-zoom-backgrounds-disney-1586899397.jpg', Bucket="skybnb", Key='test1')
-  print(uploaded_photo)
+    image = request.files["image"]
+
+    if not allowed_file(image.filename):
+        return {"errors": "file type not permitted"}, 400
+
+    image.filename = get_unique_filename(image.filename)
+
+    upload = upload_file_to_s3(image)
+
+    if "url" not in upload:
+        # if the dictionary doesn't have a url key
+        # it means that there was an error when we tried to upload
+        # so we send back that error message
+        return upload, 400
+
+    url = upload["url"]
+    # flask_login allows us to get the current user from the request
+    new_image = Picture(user=current_user, url=url)
+    db.session.add(new_image)
+    db.session.commit()
+    return {"url": url}
