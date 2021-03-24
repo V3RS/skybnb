74a389cff60d88f1dd890a701be1e6782fe512f3
Veer <40777269+V3RS@users.noreply.github.com>
2021-03-23 23:46:11 -0400

    sbasic of the title

--- app/models/spot.py
+++ app/models/spot.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import relationship
 
 class Spot(db.Model):
     __tablename__ = 'spots'
-
+    # need to add rating to this model
     id = db.Column(db.Integer, primary_key=True, nullable=False)
     title = db.Column(db.String(255), nullable=False, unique=True)
     lng = db.Column(db.Numeric, nullable=False)
--- react-app/src/components/SpotPage/index.js
+++ react-app/src/components/SpotPage/index.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
-import "./SpotPage.css";
 import { useParams } from "react-router";
 import { getOneSpot, getSpotPictures } from "../../services/spot";
+
+import "./SpotPage.css";
 
 export default function SpotPage() {
   const { spotId } = useParams();
@@ -9,13 +10,13 @@ export default function SpotPage() {
   const [pictures, setPictures] = useState([]);
   // this needs to be fixed
   useEffect(async () => {
-    // const fetchData = async () => {
-    const data = await getOneSpot(spotId);
-    const pictureData = await getSpotPictures(spotId);
-    setSpot(data);
-    setPictures(pictureData.pictures);
-    // };
-    // fetchData();
+    const fetchData = async () => {
+      const data = await getOneSpot(spotId);
+      const pictureData = await getSpotPictures(spotId);
+      setSpot(data);
+      setPictures(pictureData.pictures);
+    };
+    fetchData();
   }, []);
 
   console.log("SPOTT", spot);
@@ -25,20 +26,30 @@ export default function SpotPage() {
     <div id="spc">
       <div className="spot-page-container">
         <div className="spot-page-title">
-          <h1>{spot?.title}</h1>
-          {pictures.map((picture) => (
+          <div>{spot?.title}</div>
+        </div>
+        <div className="spot-secondHeading">
+          <div className="rating__address">
+            <div>
+              <i className="fas fa-star"></i> 4.82
+            </div>
+            <div className="spot__address">{spot?.address}</div>
+          </div>
+          <div className="spot-shbtns-container">
+            <button className="spot-shbtns">
+              <i class="far fa-share-square"></i>Share
+            </button>
+            <button className="spot-shbtns">
+              <i class="far fa-heart"></i>Save
+            </button>
+          </div>
+        </div>
+        <div className="spot__pictures__container">
+          {pictures?.map((picture) => (
             <div key={picture.id}>
               <img src={picture.img_url} alt="spot-picture" />
             </div>
           ))}
-        </div>
-        <div className="spot-secondHeading">
-          <h3>Superhost</h3>
-          <h3>City, State, Country</h3>
-          <div className="spot-shbtns">
-            <button>Share</button>
-            <button>Save</button>
-          </div>
         </div>
       </div>
     </div>
