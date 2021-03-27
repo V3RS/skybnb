886caffc6ef55aa404329609508dffc0e524d0cc
cygnus33065 <cygnus_33065@yahoo.com>
2021-03-27 18:37:55 -0400

    fixed some seeds

--- app/seeds/reviews.py
+++ app/seeds/reviews.py
@@ -9,19 +9,19 @@ def seed_reviews():
     review3 = Review(
         comment="My favorite place to stay listet on skybnb, royal amenities, full service, great defensive location, no sith lords visible", user_id=1, rating=5, spot_id=3)
     review4 = Review(
-        comment="My favorite place to stay listet on skybnb, royal amenities, full service, great defensive location, no sith lords visible", user_id=1, rating=5, spot_id=3)
+        comment="My favorite place to stay listet on skybnb, royal amenities, full service, great defensive location, no sith lords visible", user_id=1, rating=5, spot_id=4)
     review5 = Review(
-        comment="My favorite place to stay listet on skybnb, royal amenities, full service, great defensive location, no sith lords visible", user_id=1, rating=5, spot_id=3)
+        comment="My favorite place to stay listet on skybnb, royal amenities, full service, great defensive location, no sith lords visible", user_id=1, rating=5, spot_id=5)
     review6 = Review(
-        comment="My favorite place to stay listet on skybnb, royal amenities, full service, great defensive location, no sith lords visible", user_id=1, rating=5, spot_id=3)
+        comment="My favorite place to stay listet on skybnb, royal amenities, full service, great defensive location, no sith lords visible", user_id=1, rating=5, spot_id=6)
     review7 = Review(
-        comment="My favorite place to stay listet on skybnb, royal amenities, full service, great defensive location, no sith lords visible", user_id=1, rating=5, spot_id=3)
+        comment="My favorite place to stay listet on skybnb, royal amenities, full service, great defensive location, no sith lords visible", user_id=1, rating=5, spot_id=7)
     review8 = Review(
-        comment="My favorite place to stay listet on skybnb, royal amenities, full service, great defensive location, no sith lords visible", user_id=1, rating=5, spot_id=3)
+        comment="My favorite place to stay listet on skybnb, royal amenities, full service, great defensive location, no sith lords visible", user_id=1, rating=5, spot_id=8)
     review9 = Review(
-        comment="My favorite place to stay listet on skybnb, royal amenities, full service, great defensive location, no sith lords visible", user_id=1, rating=5, spot_id=3)
+        comment="My favorite place to stay listet on skybnb, royal amenities, full service, great defensive location, no sith lords visible", user_id=1, rating=5, spot_id=9)
     review10 = Review(
-        comment="My favorite place to stay listet on skybnb, royal amenities, full service, great defensive location, no sith lords visible", user_id=1, rating=5, spot_id=3)
+        comment="My favorite place to stay listet on skybnb, royal amenities, full service, great defensive location, no sith lords visible", user_id=1, rating=5, spot_id=10)
 
     db.session.add(review1)
     db.session.add(review2)
@@ -41,19 +41,19 @@ def seed_reviews():
     review13 = Review(
         comment="LOVE this place!!! Plush decors and the bedding is so soft and clean! Great Palace fit for a star princess", user_id=2, rating=5, spot_id=3)
     review14 = Review(
-        comment="LOVE this place!!! Plush decors and the bedding is so soft and clean! Great Palace fit for a star princess", user_id=2, rating=5, spot_id=3)
+        comment="LOVE this place!!! Plush decors and the bedding is so soft and clean! Great Palace fit for a star princess", user_id=2, rating=5, spot_id=4)
     review15 = Review(
-        comment="LOVE this place!!! Plush decors and the bedding is so soft and clean! Great Palace fit for a star princess", user_id=2, rating=5, spot_id=3)
+        comment="LOVE this place!!! Plush decors and the bedding is so soft and clean! Great Palace fit for a star princess", user_id=2, rating=5, spot_id=5)
     review16 = Review(
-        comment="LOVE this place!!! Plush decors and the bedding is so soft and clean! Great Palace fit for a star princess", user_id=2, rating=5, spot_id=3)
+        comment="LOVE this place!!! Plush decors and the bedding is so soft and clean! Great Palace fit for a star princess", user_id=2, rating=5, spot_id=6)
     review17 = Review(
-        comment="LOVE this place!!! Plush decors and the bedding is so soft and clean! Great Palace fit for a star princess", user_id=2, rating=5, spot_id=3)
+        comment="LOVE this place!!! Plush decors and the bedding is so soft and clean! Great Palace fit for a star princess", user_id=2, rating=5, spot_id=7)
     review18 = Review(
-        comment="LOVE this place!!! Plush decors and the bedding is so soft and clean! Great Palace fit for a star princess", user_id=2, rating=5, spot_id=3)
+        comment="LOVE this place!!! Plush decors and the bedding is so soft and clean! Great Palace fit for a star princess", user_id=2, rating=5, spot_id=8)
     review19 = Review(
-        comment="LOVE this place!!! Plush decors and the bedding is so soft and clean! Great Palace fit for a star princess", user_id=2, rating=5, spot_id=3)
+        comment="LOVE this place!!! Plush decors and the bedding is so soft and clean! Great Palace fit for a star princess", user_id=2, rating=5, spot_id=9)
     review20 = Review(
-        comment="LOVE this place!!! Plush decors and the bedding is so soft and clean! Great Palace fit for a star princess", user_id=2, rating=5, spot_id=3)
+        comment="LOVE this place!!! Plush decors and the bedding is so soft and clean! Great Palace fit for a star princess", user_id=2, rating=5, spot_id=10)
     db.session.add(review11)
     db.session.add(review12)
     db.session.add(review13)
